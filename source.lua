044c466c53b96f589aeaada3f27a1af976e0b613
ieufhosivdlkjv <168236812+ieufhosivdlkjv@users.noreply.github.com>
2024-11-22 20:18:38 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -495,9 +495,9 @@ getgenv().Main = Main
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ieufhosivdlkjv/UI-Library/refs/heads/main/uisetting.txt"))()
 gethiddenstuff.MakeCustomOutline(getgenv().Main)
 gethiddenstuff.MakeCustomTopBar(getgenv().Main)
-gethiddenstuff.MakeCustomTabs(getgenv().Main)
-gethiddenstuff.MakeCustomToggles(getgenv().Main)
-gethiddenstuff.MakeCustomSliders(getgenv().Main)
+--gethiddenstuff.MakeCustomTabs(getgenv().Main)
+--gethiddenstuff.MakeCustomToggles(getgenv().Main)
+--gethiddenstuff.MakeCustomSliders(getgenv().Main)
 -- Variables
 
 local request = (syn and syn.request) or (http and http.request) or http_request
